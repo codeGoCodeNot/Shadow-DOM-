2197e9b889eb015bf5407ab6d50772cf9500e4e3
sendosend <johnsenberdin2930@gmail.com>
2025-08-11 05:20:37 +0800

    remove extra div on burger.html

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import { Burger } from './burger.js';
-import { setUpButton } from './toggle.js';
-import { MyMain } from './main.js';
+import { Burger } from "./burger.js";
+import { setUpButton } from "./toggle.js";
+import { MyMain } from "./main.js";
 
-customElements.define('my-burger', Burger);
-customElements.define('my-main', MyMain);
+customElements.define("my-burger", Burger);
+customElements.define("my-main", MyMain);
 
 setUpButton();
 
-document.addEventListener('show-profile', () => {
-  const main = document.querySelector('my-main');
+document.addEventListener("show-profile", () => {
+  const main = document.querySelector("my-main");
   if (main) {
     main.renderProfile(); // this method you define inside MyMain class
   }
